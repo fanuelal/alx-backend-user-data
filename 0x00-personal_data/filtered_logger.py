3a02e6e21db9cfdc22cb650690d1b06301d3c073
fanuelal <fanuelalmawstop@gmail.com>
2022-05-12 09:54:27 -0700

    pycodestyle fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,11 @@
 import logging
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     """returns log message"""
     tempMessage = message
     for field in fields:
         tempMessage = re.sub(field + "=.*?" + separator,
-                      field + "=" + redaction + separator, tempMessage)
+                             field + "=" + redaction + separator, tempMessage)
     return tempMessage
