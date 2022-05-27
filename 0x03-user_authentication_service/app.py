1700beaaa42f7f0be1afae1a5c269f808e149932
fanuelal <fanuelalmawstop@gmail.com>
2022-05-27 20:11:47 +0000

    flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module flask"""
+from flask import Flask, jsonify, request, abort, redirect, url_for
+
+
+app = Flask(__name__)
+AUTH = auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """json payload of the from"""
+    jsn = jsonify({"message": "Bienvenue"})
+    return jsn
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
