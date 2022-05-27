808a6fab5d9997a29aaad09051cd566d23d1d116
fanuelal <fanuelalmawstop@gmail.com>
2022-05-27 20:22:14 +0000

    codestyle fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
-    """ GET /  JSON payload
-    """
+    """GET /  JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
